fb4bffe68ec3aa24bc5cf3811186efe6ad565cdf
BuffelSaft <buffalojuice@gmail.com>
2020-04-11 23:40:53 +1200

    Increase obedience levels
    May need to further increase these later.

--- src/battle_util.c
+++ src/battle_util.c
@@ -4820,14 +4820,14 @@ u8 IsMonDisobedient(void)
         if (FlagGet(FLAG_BADGE08_GET))
             return 0;
 
-        obedienceLevel = 10;
+        obedienceLevel = 25;
 
         if (FlagGet(FLAG_BADGE02_GET))
-            obedienceLevel = 30;
+            obedienceLevel = 40;
         if (FlagGet(FLAG_BADGE04_GET))
-            obedienceLevel = 50;
+            obedienceLevel = 60;
         if (FlagGet(FLAG_BADGE06_GET))
-            obedienceLevel = 70;
+            obedienceLevel = 80;
     }
 
     if (gBattleMons[gBattlerAttacker].level <= obedienceLevel)
