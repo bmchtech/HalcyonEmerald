39ad86a8f4af51732a5d47f331c297c5fa9b3d5e
BuffelSaft <buffalojuice@gmail.com>
2020-07-05 02:11:42 +1200

    Modernize bonus Premier Balls
    Player gets free Premier Balls for buying any kind of ball, and gets one for every ten balls bought.

--- src/shop.c
+++ src/shop.c
@@ -1134,9 +1134,16 @@ static void Task_ReturnToItemListAfterItemPurchase(u8 taskId)
     if (gMain.newKeys & (A_BUTTON | B_BUTTON))
     {
         PlaySE(SE_SELECT);
-        if (tItemId == ITEM_POKE_BALL && tItemCount > 9 && AddBagItem(ITEM_PREMIER_BALL, 1) == TRUE)
+        if ((ItemId_GetPocket(tItemId) == POCKET_POKE_BALLS) && tItemCount > 9 && AddBagItem(ITEM_PREMIER_BALL, tItemCount / 10) == TRUE)
         {
-            BuyMenuDisplayMessage(taskId, gText_ThrowInPremierBall, BuyMenuReturnToItemList);
+            if (tItemCount > 19)
+            {
+                BuyMenuDisplayMessage(taskId, gText_ThrowInPremierBalls, BuyMenuReturnToItemList);
+            }
+            else
+            {
+                BuyMenuDisplayMessage(taskId, gText_ThrowInPremierBall, BuyMenuReturnToItemList);
+            }
         }
         else if((ItemId_GetPocket(tItemId) == POCKET_TM_HM))
         {
