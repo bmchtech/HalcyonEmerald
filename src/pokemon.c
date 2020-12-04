b92a7e05bfadb4431889ec99346f18b2fe60ef71
BuffelSaft <buffalojuice@gmail.com>
2020-12-05 01:21:07 +1300

    Victory Road, Ever Grande tweaks
    Fix Scott, change some items, give Wally the rival theme.

--- src/pokemon.c
+++ src/pokemon.c
@@ -6744,10 +6744,8 @@ u16 GetBattleBGM(void)
         case TRAINER_CLASS_CHAMPION:
             return MUS_VS_CHAMPION;
         case TRAINER_CLASS_PKMN_TRAINER_3:
-            if (gBattleTypeFlags & BATTLE_TYPE_FRONTIER)
-                return MUS_VS_RIVAL;
-            if (!StringCompare(gTrainers[gTrainerBattleOpponent_A].trainerName, gText_BattleWallyName))
-                return MUS_VS_TRAINER;
+            //if (!StringCompare(gTrainers[gTrainerBattleOpponent_A].trainerName, gText_BattleWallyName))
+                //return MUS_RG_VS_CHAMPION;
             return MUS_VS_RIVAL;
         case TRAINER_CLASS_ELITE_FOUR:
             return MUS_VS_ELITE_FOUR;
