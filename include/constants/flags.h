ddc328f37ed9334b49155315d600dc8cd581646e
BuffelSaft <buffalojuice@gmail.com>
2020-12-20 13:53:07 +1300

    OW TMs use gold ball sprite
    Also add missing Shadow Claw TM to Granite Cave.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -511,7 +511,7 @@
 #define FLAG_SHOALCAVE_SLOWBRONITE           0x1DF
 #define FLAG_ROUTE118_GYARADOSITE            0x1E0
 #define FLAG_SCORCHED_SLAB_TYRANITARITE      0x1E1
-#define FLAG_UNUSED_0x1E2                    0x1E2 // Unused Flag
+#define FLAG_ITEM_GRANITE_CAVE_B1F_TM65      0x1E2
 #define FLAG_UNUSED_0x1E3                    0x1E3 // Unused Flag
 
 // Mystery Gift Flags (Unknown)
