3cfda4b2c88069654161b6d247412c3d331dd4c8
BuffelSaft <buffalojuice@gmail.com>
2021-02-14 14:49:31 +1300

    Define save space for fused Pokemon
    Once gen 8 mons are added this will future proof saves, preventing major compatibility issues in later versions.

--- include/global.h
+++ include/global.h
@@ -999,6 +999,9 @@ struct SaveBlock1
     /*0x3???*/ u8 registeredTexts[UNION_ROOM_KB_ROW_COUNT][21];
     /*0x3???*/ struct SaveTrainerHill trainerHill;
     /*0x3???*/ struct WaldaPhrase waldaPhrase;
+    /*0x3???*/ struct BoxPokemon kyuremFusedMon;
+    /*0x3???*/ struct BoxPokemon necrozmaFusedMon;
+    /*0x3???*/ struct BoxPokemon calyrexFusedMon;
     // sizeof: 0x3???
 };
 
