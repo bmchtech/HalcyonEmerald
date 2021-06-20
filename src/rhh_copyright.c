a82c56be36b2a97b138fa65bc7dca60ad3386d44
Jaizu <jaizu85@gmail.com>
2021-06-20 18:23:28 +0200

    Porygon recovered his voice

--- src/rhh_copyright.c
+++ src/rhh_copyright.c
@@ -328,6 +328,7 @@ static void SpriteCallback_PorygonFlying(struct Sprite* sprite)
             sprite->callback = SpriteCallback_PorygonHit;
             sprite->sTimer = 0;
             PlaySE(SE_M_DOUBLE_SLAP);
+            PlayCryInternal(SPECIES_PORYGON, 0, 120, 10, 0);
         }
     }
     sprite->sTimer++;
