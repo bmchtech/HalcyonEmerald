3c6200be9ab51260d606500e39954b040f367fa2
redthing1 <xhyinne@gmail.com>
2021-12-25 16:35:15 -0800

    run indoors patch

--- src/bike.c
+++ src/bike.c
@@ -890,7 +890,7 @@ static u8 GetBikeCollisionAt(struct ObjectEvent *objectEvent, s16 x, s16 y, u8 d
 
 bool8 RS_IsRunningDisallowed(u8 tile)
 {
-    if (IsRunningDisallowedByMetatile(tile) != FALSE || gMapHeader.mapType == MAP_TYPE_INDOOR)
+    if (IsRunningDisallowedByMetatile(tile))
         return TRUE;
     else
         return FALSE;
