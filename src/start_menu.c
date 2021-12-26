003f3154f1eeede6690744bd269fdbd0f6e0a1aa
redthing1 <xhyinne@gmail.com>
2021-12-25 16:42:36 -0800

    dont confirm overwriting same file

--- src/start_menu.c
+++ src/start_menu.c
@@ -1014,7 +1014,7 @@ static u8 SaveFileExistsCallback(void)
     }
     else
     {
-        ShowSaveMessage(gText_AlreadySavedFile, SaveConfirmOverwriteCallback);
+        sSaveDialogCallback = SaveSavingMessageCallback;
     }
 
     return SAVE_IN_PROGRESS;
