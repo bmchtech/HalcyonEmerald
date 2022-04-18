7e30c60f3b6de05b2ea5d46c5fc4c8a3c8ad290f
redthing1 <redthing1@alt.icu>
2022-04-18 12:46:10 -0700

    move tutors don't use flags and are infinite

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -463,16 +463,16 @@
 #define FLAG_SHOWN_EON_TICKET                0x1AE
 #define FLAG_SHOWN_AURORA_TICKET             0x1AF
 #define FLAG_SHOWN_OLD_SEA_MAP               0x1B0
-#define FLAG_MOVE_TUTOR_TAUGHT_SWAGGER       0x1B1
-#define FLAG_MOVE_TUTOR_TAUGHT_ROLLOUT       0x1B2
-#define FLAG_MOVE_TUTOR_TAUGHT_FURY_CUTTER   0x1B3
-#define FLAG_MOVE_TUTOR_TAUGHT_MIMIC         0x1B4
-#define FLAG_MOVE_TUTOR_TAUGHT_METRONOME     0x1B5
-#define FLAG_MOVE_TUTOR_TAUGHT_SLEEP_TALK    0x1B6
-#define FLAG_MOVE_TUTOR_TAUGHT_SUBSTITUTE    0x1B7
-#define FLAG_MOVE_TUTOR_TAUGHT_DYNAMICPUNCH  0x1B8
-#define FLAG_MOVE_TUTOR_TAUGHT_DOUBLE_EDGE   0x1B9
-#define FLAG_MOVE_TUTOR_TAUGHT_EXPLOSION     0x1BA
+#define FLAG_UNUSED_0x1B1                    0x1B1 // Unused Flag
+#define FLAG_UNUSED_0x1B2                    0x1B2 // Unused Flag
+#define FLAG_UNUSED_0x1B3                    0x1B3 // Unused Flag
+#define FLAG_UNUSED_0x1B4                    0x1B4 // Unused Flag
+#define FLAG_UNUSED_0x1B5                    0x1B5 // Unused Flag
+#define FLAG_UNUSED_0x1B6                    0x1B6 // Unused Flag
+#define FLAG_UNUSED_0x1B7                    0x1B7 // Unused Flag
+#define FLAG_UNUSED_0x1B8                    0x1B8 // Unused Flag
+#define FLAG_UNUSED_0x1B9                    0x1B9 // Unused Flag
+#define FLAG_UNUSED_0x1BA                    0x1BA // Unused Flag
 #define FLAG_DEFEATED_REGIROCK               0x1BB
 #define FLAG_DEFEATED_REGICE                 0x1BC
 #define FLAG_DEFEATED_REGISTEEL              0x1BD
